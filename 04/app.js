3aa356deacbc46811f3f96720eb19028d794bfeb
Martin Gawlyta <martin.guide@gmail.com>
2020-06-27 16:09:55 +0200

    3/4 done

--- 04/app.js
+++ 04/app.js
@@ -3,30 +3,33 @@
 // ************
 
 const divEl = document.querySelectorAll('div');
+
+// let timeout = divEl.forEach(e => {
+//   temp = e.dataset.time;
+//   console.log(temp);
+// });
+
 divEl.forEach(e => {
   e.addEventListener('click', el => {
-    el.stopPropagation();
-    el.target.classList.add('clicked');
-    console.log(el.currentTarget);
+    setTimeout(function() {
+      // el.preventDefault(); // or maybe stopPropagation?
+      el.target.classList.add('clicked'); console.log(el.currentTarget);
+    }, e.dataset.time);
   });
 });
 
 // ************
-// TASK 4.01 **
+// TASK 4.02 **
 // ************
+const bodyEl = document.querySelector('body');
 
-if (el.target.classList.contains('clicked')) {
-  const bodyEl = document.querySelector('body');
-  bodyEl.addEventListener('click', el => {
-    el.target.classList.remove('clicked')
-  });
+bodyEl.addEventListener('click', removeClass);
 
-}
-// ************
-// TASK 4.01 **
-// ************
+function removeClass() {
+  const divEl = document.querySelectorAll('div');
+  divEl.forEach(el => el.classList.remove('clicked'));
+};
 
+// Uważaj na kontekst dla `this` oraz pierwszy parametr dla callback-a - możliwe, że będzie potrzebna użyć dodatkowej zmiennej.
 
-// ************
-// TASK 4.01 **
-// ************
+// o co tutaj chodzi z tym callbackiem?
