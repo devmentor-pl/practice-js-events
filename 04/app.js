a34381d1a0cb4ba58e79f393c813868381949893
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2020-07-29 16:47:05 +0200

    task 4 done

--- 04/app.js
+++ 04/app.js
@@ -4,17 +4,21 @@ const body = document.querySelector('body');
 console.log(allDivs);
 console.log(body);
 
-function addClass(e) {
-    e.stopPropagation();
-    e.currentTarget.classList.add('clicked');
-};
+function addClass() {};
 
 function timeOut(time) {
     setTimeout(addClass, time)
 };
 
 allDivs.forEach(function(item) {
-    item.addEventListener('click', addClass);
+    console.log(item.dataset.time);
+    item.addEventListener('click', function(e) {
+        e.stopPropagation();
+        const self = e.currentTarget;
+        setTimeout(function() {
+            self.classList.add('clicked');
+        }, item.dataset.time);
+    });
     body.addEventListener('click', function(e) {
         e.stopPropagation();
         item.classList.remove('clicked');
