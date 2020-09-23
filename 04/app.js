ab79f69bb27eccce4650139d872acc79f4819632
AnnaBuller <aniabela@wp.pl>
2020-09-23 16:17:03 +0200

    solve 04 task

---
+++ 04/app.js
@@ -0,0 +1,29 @@
+function onLoadedCont() {
+  const divList = document.querySelectorAll('div');
+
+  //powinnam sprawdzać ifem czy tablica jest pusta, czy w zasadzie to niepotrzebne, bo pętla i tak nie miałaby po czym iterować?
+  divList.forEach(element => {
+    element.addEventListener('click', addClass)
+  });
+
+  // setTimeout(() => {
+  //   this.classList.add('clicked');
+  //   this.stopPropagation()
+  // }, 300)
+
+  function addClass(e) {
+    setTimeout(() => {
+      this.classList.add('clicked');
+    }, this.dataset.time)
+    e.stopPropagation()
+  }
+
+  function removeClass() {
+    divList.forEach(element => {
+      element.classList.remove('clicked')
+    });
+  }
+
+  document.body.addEventListener('click', removeClass)
+}
+document.addEventListener('DOMContentLoaded', onLoadedCont)
