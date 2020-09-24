d0c09f0bbf69ab7fc7bf48440fa0d8e86d2773ea
AnnaBuller <aniabela@wp.pl>
2020-09-24 12:00:20 +0200

    solve 05 task partly

--- 05/app.js
+++ 05/app.js
@@ -8,7 +8,32 @@ const stats = {
 };
 
 /* tutaj umieść swój kod */
+const pList = document.querySelectorAll('[data-id]');
 
+function countClick(e) {
+    e.preventDefault();
+    const hrefAttr = e.target.getAttribute('href');
+    const dataAttr = e.target.getAttribute('data-id');
+
+    if (Object.keys(stats.paragraphs).includes(dataAttr)) {
+        stats.paragraphs[dataAttr] += 1;
+    }
+    else if (Object.keys(stats.links).includes(hrefAttr)) {
+        stats.links[hrefAttr] += 1;
+    } else {
+        if (e.target.tagName.includes('A')) {
+            const newLink = hrefAttr;
+            stats.links[newLink] = + 1;
+        } else {
+            const newP = dataAttr;
+            stats.links[newP] = + 1; // do konsultacji ;)
+        }
+    }
+}
+
+pList.forEach(p => {
+    p.addEventListener('click', countClick)
+})
 
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
@@ -26,9 +51,11 @@ const fireCustomEvent = function(element, name) {
 const renderStats = function(data, element) {
     let html = '';
     for(let elementType in data) {
+        //dla każdego obiektu w stats tworzysz <ul>
         html += '<ul>';
 
         for(let key in data[elementType]) {
+            // w każdym obiekcie stats dla właściwości w nim zawartych tworzysz <li> z nazwą właściwości i jej wartością 
             
             html += '<li>';
             html += key + ' -> ' + data[elementType][key];
@@ -47,7 +74,8 @@ document.addEventListener('click', function(e) {
     if(tagName.includes('P') || tagName.includes('A')) {
         fireCustomEvent(statsElement, 'render');
     }
-});
-statsElement.addEventListener('render', renderStats.bind(this, stats, statsElement));
-document.addEventListener('DOMContentLoaded', fireCustomEvent(statsElement, 'render'));
+}); //sprawdzamy czy kliknęliśmy na link lub paragraf - jeśli tak, to odpalamy funkcję odpalającą customowy event, który uruchomi funkcję wyświetlającą statystyki
+statsElement.addEventListener('render', renderStats.bind(this, stats, statsElement)); //wywołanie funckji renderStats, która tutaj generuje listę statystyk z wykorzystaniem obiektu stats i wrzuca dzieci do elementu HTML (section klasy .stats)
+document.addEventListener('DOMContentLoaded', fireCustomEvent(statsElement, 'render')); //gdy załadujesz DOM, odpal funkcję fireCustomEvent, która odpala event i odpowiada za wyświetlanie section => 'render' w konsoli.
+//Odpalony event uruchamia funkcję renderStats.bind powyżej
 
