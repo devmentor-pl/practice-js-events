1c5889800757dc22f2a6d4356229dfe95da0d880
AnnaBuller <aniabela@wp.pl>
2020-09-28 11:53:43 +0200

    remove unneces if

--- 03/app.js
+++ 03/app.js
@@ -1,19 +1,14 @@
 function onLoadedCont() {
   const btnList = document.querySelectorAll('button')
-
-  if (btnList.length > 0) {
     btnList.forEach(item => {
       item.addEventListener('click', addNewText);
     });
-  }
 }
 
 function addNewText() {
   this.innerText = 'clicked';
   console.log('clicked')
-  if (this.innerText === 'clicked') {
-    this.removeEventListener('click', addNewText)
-  }
+  this.removeEventListener('click', addNewText)
 }
 
 document.addEventListener('DOMContentLoaded', onLoadedCont)
