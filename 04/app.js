bc8c9d9d1cbeff7e17644c61d5e501774e3ba0c8
AnnaBuller <aniabela@wp.pl>
2020-09-28 12:47:03 +0200

    remove comments

--- 04/app.js
+++ 04/app.js
@@ -1,7 +1,6 @@
 function onLoadedCont() {
   const divList = document.querySelectorAll('div');
 
-  //powinnam sprawdzać ifem czy tablica jest pusta, czy w zasadzie to niepotrzebne, bo pętla i tak nie miałaby po czym iterować?
   divList.forEach(element => {
     element.addEventListener('click', addClass)
   });
@@ -15,7 +14,6 @@ function onLoadedCont() {
     setTimeout(() => {
       this.classList.add('clicked');
     }, this.dataset.time)
-    // e.stopPropagation() // zadanie nie precyzuje, czy po kliknięciu na najbardziej zagnieżdżony div, chcemy zakolorować wszystkie prostokąty, czy jednak każdy z osobna. Jeśli to drugie, to należałoby odkomentować e.stopPropagation()
   }
 
   function removeClass() {
