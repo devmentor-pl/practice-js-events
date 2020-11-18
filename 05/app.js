4f25740d2f079008a2dc77d4d2c03fd881650e0b
gregor232 <ktotok@gmail.com>
2020-11-18 16:04:24 +0100

    task 5 done

--- 05/app.js
+++ 05/app.js
@@ -10,25 +10,66 @@ const stats = {
 /* tutaj umieść swój kod */
 const paragraphs = document.querySelectorAll("p");
 for (let i = 0; i < paragraphs.length; i++) {
-paragraphs[i].addEventListener("click", function () {
-        if (paragraphs[i].dataset.id === "p1") {
+paragraphs[i].addEventListener("click", function (e) {
+        if (e.target.dataset.id === "p1") {
             stats.paragraphs.p1 = stats.paragraphs.p1 + 1;
+        }
+        if (e.target.dataset.id === "p2") {
+            if(!stats.paragraphs.p2) {
+                stats.paragraphs.p2 = 0;
+            }
+            
+            stats.paragraphs.p2 = stats.paragraphs.p2 + 1;
+            
+        }
+        if (e.target.dataset.id === "p3") {
+            if(!stats.paragraphs.p3) {
+                stats.paragraphs.p3 = 0;
+            }
+            
+            stats.paragraphs.p3 = stats.paragraphs.p3 + 1;
+            
         }
 }
 )}
 const els = document.querySelectorAll("a[href='/dolor.html']");
 els.forEach(function (element) {
-    element.onclick = function() {
+    element.onclick = function(e) {
+        console.log(e.target, e.currentTarget);
         stats.links["/dolor.html"] = stats.links["/dolor.html"] + 1;
         element.href = "#";
 
     }
 })
+const elsElite = document.querySelectorAll("a[href='/adipisicing-elite.html']");
+elsElite.forEach(function (element) {
+    
+    element.onclick = function(e) {
+        if(!stats.links['/adipisicing-elite.html']) {
+            stats.links['/adipisicing-elite.html'] = 0;
+        }
+        console.log(e.target, e.currentTarget);
+        stats.links['/adipisicing-elite.html'] = stats.links['/adipisicing-elite.html'] + 1;
+        element.href = "#";
 
+    }
+})
+
+const consectetur = document.querySelector("a[href='/consectetur.html']");
+consectetur.onclick = function(e) {
+    if(!stats.links['/consectetur.html']) {
+        stats.links['/consectetur.html'] = 0;
+    }
+    console.log(e.target, e.currentTarget);
+    stats.links['/consectetur.html'] = stats.links['/consectetur.html'] + 1;
+    consectetur.href = "#";
+
+}
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
-const statsElement = document.querySelector('.stats');
-const fireCustomEvent = function(element, name) {
+const statsElement = document.querySelector('.stats');// wyszukanie elementu o klasie stat
+const fireCustomEvent = function(element, name) { // bardzo uniwersalna funkcja ktora bedzie urochamiac nasz customowy event. element to element htmlowy na ktorym ma to byc event
+    // a name to jego nazwa
     console.log(element, '=>', name);
 
     const event = new CustomEvent(name, { // tworzy swoj event
@@ -40,7 +81,7 @@ const fireCustomEvent = function(element, name) {
 
 const renderStats = function(data, element) {
     let html = '';
-    for(let elementType in data) {
+    for(let elementType in data) {// iteruje po elementach obiektu stats
         html += '<ul>';
 
         for(let key in data[elementType]) {
@@ -60,9 +101,9 @@ const renderStats = function(data, element) {
 document.addEventListener('click', function(e) {
     const tagName = e.target.tagName;
     if(tagName.includes('P') || tagName.includes('A')) {
-        fireCustomEvent(statsElement, 'render');
+        fireCustomEvent(statsElement, 'render'); // wywoluje event render na statsElement
     }
 });
-statsElement.addEventListener('render', renderStats.bind(this, stats, statsElement));
+statsElement.addEventListener('render', renderStats.bind(this, stats, statsElement));// kiedy event render nastapi, ma uchoromic sie renderStats
 document.addEventListener('DOMContentLoaded', fireCustomEvent.bind(null, statsElement, 'render'));
 
