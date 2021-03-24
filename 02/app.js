d4cc9ff7c6fa2bac3ca035272e1ab3cd1ce6d4cc
Agnieszka Polińska <agapoli@gmail.com>
2021-03-24 16:12:07 +0100

    Mouse events

---
+++ 02/app.js
@@ -0,0 +1,3 @@
+const square = document.querySelector('div');
+square.addEventListener('mousemove', () => console.log('mousemove'));
+square.addEventListener('mouseleave', () => console.log('mouseleave'));
