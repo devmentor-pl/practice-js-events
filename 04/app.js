0a70848e0365db8fa1bb07b60db5a6edc77142ce
Agnieszka Polińska <agapoli@gmail.com>
2021-03-24 16:28:22 +0100

    Add setTimeOut()

--- 04/app.js
+++ 04/app.js
@@ -3,7 +3,15 @@ const boxes = document.querySelectorAll('div');
 boxes.forEach(box => {
     box.addEventListener('click', (e) => {
         e.stopPropagation();
-        e.currentTarget.classList.add('clicked')
+        const currBox = e.currentTarget;
+
+        if (currBox.classList.contains('child')) {
+            boxes.forEach(box => {
+                setTimeout(() => box.classList.add('clicked'), box.dataset.time)
+            });
+        } else {
+            setTimeout(() => currBox.classList.add('clicked'), currBox.dataset.time);
+        }
     })
 })
 
