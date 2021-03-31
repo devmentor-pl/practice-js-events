7795eb19fc8c44c83c796c2735a20c45e44960b1
Agnieszka Polińska <agapoli@gmail.com>
2021-03-31 09:41:00 +0200

    Handle if paragraph was clicked

--- 05/app.js
+++ 05/app.js
@@ -14,14 +14,12 @@ pList.forEach(par => {
     par.addEventListener('click', (e) => {
         e.preventDefault();
 
-        console.log(e.target)
-        console.log(e.target.pathname)
-
         if (e.target.tagName === 'A') {
-
             stats.links[e.target.pathname] = !stats.links[e.target.pathname] ? stats.links[e.target.pathname] = 1 : stats.links[e.target.pathname] = stats.links[e.target.pathname] + 1;
 
             stats.paragraphs[e.target.parentElement.dataset.id] = !stats.paragraphs[e.target.parentElement.dataset.id] ? stats.paragraphs[e.target.parentElement.dataset.id] = 1 : stats.paragraphs[e.target.parentElement.dataset.id] + 1; 
+        } else {
+            stats.paragraphs[e.target.dataset.id] = !stats.paragraphs[e.target.dataset.id] ? stats.paragraphs[e.target.dataset.id] = 1 : stats.paragraphs[e.target.dataset.id] + 1;
         }
     })
 })
