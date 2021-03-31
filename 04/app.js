9ccb35e72f58ce5c373a21d970cfc0021b461106
Agnieszka Polińska <agapoli@gmail.com>
2021-03-31 09:20:04 +0200

    Resolve without e.stopPagination()

--- 04/app.js
+++ 04/app.js
@@ -2,19 +2,14 @@ const boxes = document.querySelectorAll('div');
 
 boxes.forEach(box => {
     box.addEventListener('click', (e) => {
-        e.stopPropagation();
         const currBox = e.currentTarget;
-
-        if (currBox.classList.contains('child')) {
-            boxes.forEach(box => {
-                setTimeout(() => box.classList.add('clicked'), box.dataset.time)
-            });
-        } else {
-            setTimeout(() => currBox.classList.add('clicked'), currBox.dataset.time);
-        }
+        setTimeout(() => currBox.classList.add('clicked'), currBox.dataset.time);
     })
 })
 
-document.querySelector('body').addEventListener('click', () => {
-    boxes.forEach(box => box.classList.remove('clicked'))
+document.querySelector('body').addEventListener('click', (e) => {
+    console.log(e.target.tagName)
+    if (e.target.tagName === "BODY") {
+        boxes.forEach(box => box.classList.remove('clicked'))
+    }
 })
