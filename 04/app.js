36add6523023c3829d4107788a8d0218eac43b74
patrickw7 <patrykw3@o2.pl>
2021-05-01 17:38:22 +0200

    commit

--- 04/app.js
+++ 04/app.js
@@ -1,12 +1,11 @@
 //1
 const divCollection = [...document.querySelectorAll('div')];
 
-// divCollection.forEach((div) => {
-//     div.addEventListener('click', function() {
-//         this.classList.add('clicked');
-//     })
-// });
+// function changeColor()  {
+//    this.classList.add('clicked')
+// }
 
+// divCollection.forEach(div => div.addEventListener('click', changeColor))
 
 //2
 const body = document.querySelector('body');
@@ -22,21 +21,15 @@ body.addEventListener('click', removeColor)
 
 //3
 
-const divChild = document.querySelector('div.child')
+function colorTimer() {
+    const that = this
+    const time = this.dataset.time * 1;
+   setTimeout(function() {
+that.classList.add('clicked')
+   }, time );
+    
+}
 
 
 
-// divChild.addEventListener('click', function() {
-//     const that = this
-//     const time = (this.dataset.time)*1
-//     setTimeout(time, this.classList.add('clicked'));
-// })
-
-divChild.addEventListener('click', function () {
-    divCollection.forEach((div) => {
-        time = div.dataset.time *1
-        setTimeout(time, div.classList.add('clicked'))
-    })
-//   const time = this.dataset.time * 1;
-//   setTimeout(time, this.classList.add('clicked'));
-});
+divCollection.forEach((div) => div.addEventListener('click', colorTimer));
