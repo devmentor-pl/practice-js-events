75e3c71d753922c49029cfa15ba80fe12e5553b4
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-06-14 17:36:08 +0200

    task 01 - update

--- 01/app.js
+++ 01/app.js
@@ -1,3 +1,19 @@
-document.querySelector('button').addEventListener('click' , function() {
-    console.log('clicked');
+//  Rozwiązanie 1
+
+// document.querySelector('button').addEventListener('click' , function() {
+//     console.log('clicked');
+// })
+
+
+//  Rozwiązanie 2
+
+
+const btn = document.querySelectorAll('button');
+
+const addConsoleLog = function() {
+  console.log('clicked');
+}
+
+btn.forEach( function(element) {
+    element.addEventListener('click' , addConsoleLog);
 })
