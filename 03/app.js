a6b33fb5918c928db3b575eb1574c9f4de6210fa
Malgorzata Kowacka <kowackag@gmail.com>
2021-06-18 11:15:34 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -4,7 +4,7 @@ const changeText = function () {
     const text = this.innerText;
     this.innerText = 'clicked';
     console.log(text);
-    this.removeEventListener('click', changeText)
+    this.removeEventListener('click', changeText);
 }
 
 btnList.forEach(function (item) {
