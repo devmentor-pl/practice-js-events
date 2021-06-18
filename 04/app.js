f674463a930f03bd64597b3de9aded2bae9c5aa7
Malgorzata Kowacka <kowackag@gmail.com>
2021-06-18 13:10:59 +0200

    Finish task 04.02

--- 04/app.js
+++ 04/app.js
@@ -3,7 +3,17 @@ const divList = document.querySelectorAll('div');
 divList.forEach(function (item) {
     item.addEventListener('click', addClass)
 })
+document.body.addEventListener('click', removeClass);
 
-function addClass() {
+function addClass(e) {
     this.classList.add('clicked');
+    if (this.tagName === 'DIV') {
+        e.stopPropagation();
+    }
+}
+
+function removeClass() {
+    divList.forEach(function (item) {
+        item.classList.remove('clicked')
+    })
 }
