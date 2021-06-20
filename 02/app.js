f167779717cedc055b539a3a50a407d59ba97dbd
Malgorzata Kowacka <kowackag@gmail.com>
2021-06-20 09:33:14 +0200

    Cahnge task 02
    Add condition if (divElement)

--- 02/app.js
+++ 02/app.js
@@ -1,14 +1,16 @@
 // const init = function() {
 //     const 
 // }
-const init = function () {  //założyłam, że nie można zmieniać pliku w html
+const init = function () { //założyłam, że nie można zmieniać pliku w html
     const divElement = document.querySelector('div');
-    divElement.addEventListener('mousemove', function () {
-        console.log('Mousemove on divElement')
-    });
-    divElement.addEventListener('mouseleave', function () {
-        console.log('Mouse left div element')
-    });
+    if (divElement) {
+        divElement.addEventListener('mousemove', function () {
+            console.log('Mousemove on divElement')
+        });
+        divElement.addEventListener('mouseleave', function () {
+            console.log('Mouse left div element')
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', init);
