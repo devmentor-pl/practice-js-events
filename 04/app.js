46ed3f80c5bd3e556104f3cd3d171db14b1aeec4
Andrzej <andrzej.rakowski79@gmail.com>
2021-07-07 13:20:32 +0200

    Zadanie nr 4 czesc 1 zrobiona

--- 04/app.js
+++ 04/app.js
@@ -1,13 +1,19 @@
 const divsList = document.querySelectorAll('div');
 const bodyEl = document.querySelector('body');
-console.log(bodyEl);
-console.log(divsList);
 
 const addClass = function() {
     this.classList.add('clicked');
 }
 
-
 divsList.forEach(function(el) {
     el.addEventListener('click', addClass);
 });
+
+bodyEl.addEventListener('click', function(e) {
+    console.log(this.tagName);
+    if(this.tagName === 'BODY') {
+        divsList.forEach(function(el) {
+            el.classList.remove('clicked');
+        })
+    }
+})
