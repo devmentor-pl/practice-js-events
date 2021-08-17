c8a41bb90095e0b6f16b5657062a0dfddb58146c
Jakub Chrobak <jchrobakk@gmail.com>
2021-08-17 18:40:40 +0200

    Finish task 04

---
+++ 04/app.js
@@ -0,0 +1,26 @@
+const divsList = document.querySelectorAll("div");
+
+divsList.forEach(function (div) {
+  div.addEventListener("click", function () {
+    const divEl = this;
+    addClass(divEl);
+  });
+});
+
+function addClass(target) {
+  const time = target.dataset.time;
+  setTimeout(function () {
+    target.classList.add("clicked");
+  }, time);
+}
+
+const body = document.body;
+console.log(body);
+
+body.addEventListener("click", function (e) {
+  if (e.target == document.body) {
+    divsList.forEach(function (el) {
+      el.classList.remove("clicked");
+    });
+  }
+});
