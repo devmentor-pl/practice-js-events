7ac486f8d3d62c30c43bc71ca14681a5140ea4e0
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-21 21:23:39 +0200

    Finish task 4.2

--- 04/app.js
+++ 04/app.js
@@ -1,10 +1,19 @@
 const divsArr = document.querySelectorAll('div');
 
-function addClass() {
+// 4.1
+function addClass(e) {
 	this.classList.add('clicked');
+	e.stopPropagation();
 }
-
 divsArr.forEach((el) => {
 	el.addEventListener('click', addClass);
 });
 
+// 4.2
+const body = document.querySelector('body');
+function deleteClass() {
+	divsArr.forEach((el) => {
+		el.classList.remove('clicked');
+	});
+}
+body.addEventListener('click', deleteClass);
