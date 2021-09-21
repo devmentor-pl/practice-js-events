16d8dc5441ae280db5e7de1b4164ab48665fb5d4
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2021-09-21 22:58:32 +0200

    Finish task 4.3

--- 04/app.js
+++ 04/app.js
@@ -1,12 +1,21 @@
 const divsArr = document.querySelectorAll('div');
 
 // 4.1
+/*
 function addClass(e) {
 	this.classList.add('clicked');
 	e.stopPropagation();
 }
+*/
+ 
+// 4.3
 divsArr.forEach((el) => {
-	el.addEventListener('click', addClass);
+	el.addEventListener('click', function () {
+		setTimeout(function addClass() {
+			el.classList.add('clicked');
+		}, el.getAttribute('data-time'));
+	});
+	console.log(Number(el.getAttribute('data-time')));
 });
 
 // 4.2
