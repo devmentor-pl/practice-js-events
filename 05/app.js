4dab5c917287be6b34879cd0d64a4f1735e8386d
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-10-03 00:25:53 +0200

    Done

--- 05/app.js
+++ 05/app.js
@@ -9,6 +9,57 @@ const stats = {
 
 /* tutaj umieść swój kod */
 
+//na spotkanku mam kilka pytań odośnie tego zadania :). Podejrzewam ze i tak do poprawy :<
+
+const linkElement = document.querySelectorAll('.link');
+const paragraphElement = document.querySelectorAll('.text');
+let numberP1 = 1;
+let clickParagaphCounter = 1;
+let counter = 1;
+
+const clickCounter = function(){
+
+    if (this.dataset.id === 'p1'){
+        stats.paragraphs.p1 = numberP1++;
+    }
+    console.log('Paragraf naciśnięty: ' + clickParagaphCounter++);
+}
+
+// console.log(stats.paragraphs.p1);
+// console.log(stats.links["/dolor.html"]);
+
+
+for (let i=0; i<linkElement.length; i++){
+
+    let number = 1;
+
+    if (linkElement[i].href !== null) {
+        let attribute = linkElement[i].getAttribute('href');
+        
+        linkElement[i].addEventListener('click', function(el) {
+            
+            if(attribute === '/dolor.html') {
+                el.preventDefault();
+                
+                // console.log(counter);
+                // counter++;
+                stats.links["/dolor.html"] = counter++;
+            }
+            else {
+                el.preventDefault();
+                console.log('Atrybut: ' + attribute + ' naciśnięty po raz ' + number++);
+            }
+        });
+    }
+}                            
+
+paragraphElement.forEach(function(item){
+
+    if(item.dataset !==null) {
+        item.addEventListener('click', clickCounter, true);
+    }
+});
+
 
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
