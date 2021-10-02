080c26aac4178385ed2aaa0c5b5c588c91c7a231
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-10-02 16:24:04 +0200

    I hope third is done too

---
+++ 03/app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const btns = document.querySelectorAll('button');
+    const clickEvent = function(el){
+
+        
+        this.innerText = 'clicked';
+        
+        if(this.innerText === 'clicked') {
+            el.stopImmediatePropagation();
+            console.log(this.innerText);
+        }
+
+    }
+
+    if(btns) {
+        btns.forEach(function(item){
+            item.addEventListener('click', clickEvent);
+        })
+    }
+
+})
