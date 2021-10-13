2109d35bd15b9c086dca3af2e3ebbb7fbf5bd1c0
Michal Krawczyk <krawczykmichal1@gamil.com>
2021-10-13 08:33:33 +0100

    done

---
+++ 02/app.js
@@ -0,0 +1,16 @@
+const init = function(){ 
+    const div = document.querySelector('div');
+    const moveOver = function(){
+    div.textContent = 'move over';
+    console.log('move over')
+}
+    const onLeave = function(){
+    div.innerText = 'left div';
+    console.log('left div')
+}
+if(div) {
+    div.addEventListener('mouseover', moveOver)
+    div.addEventListener('mouseleave', onLeave)
+}
+}
+document.addEventListener('DOMContentLoaded', init)
