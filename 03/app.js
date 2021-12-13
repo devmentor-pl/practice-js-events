6eb43bf01a734fe0d3354951f885f50a8f5fa65f
Ewelina <ewelina@ekopacz.pl>
2021-12-13 13:50:31 +0100

    upgrade

--- 03/app.js
+++ 03/app.js
@@ -1,16 +1,9 @@
 const btnList = document.querySelectorAll('button');
-let counter = 0;
 
 let changeText = function() {
-    counter++; // tutaj jest cos nie tak ale nie wiem i chyba nie dojde do tego, pozwala mi kliknąc 5x 
-    
-    if (counter>=3) {
-        this.removeEventListener('click', changeText);
-    }
-
     this.innerText = 'clicked';
-    console.log(this.innerText);
-    console.log(counter);   
+    console.log(this.innerText); 
+    this.removeEventListener('click', changeText);
 }
 
 btnList.forEach(function(element){
