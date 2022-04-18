081030120a86cb9b62e603d96aae15e4b4042ca0
Martyna Giee <martyna.giee@gmail.com>
2022-04-18 13:15:03 +0200

    add DOMContentLoaded eventListener

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,8 @@
-(function () {
+const init = function () {
 
     const mainDiv = document.querySelector('div');
     mainDiv.addEventListener("mousemove", e => console.log('move inside div'))
     mainDiv.addEventListener("mouseleave", e => console.log('coursor left'))
+}
 
-})()
+document.addEventListener('DOMContentLoaded', init);
