4d1e96540521bb70343ee3c9a5fceeadda28b2c3
Camila Montes Celinski <camicelinski@gmail.com>
2022-04-30 15:35:20 +0200

    Finish task 04.02

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,20 @@
 const boxList = document.querySelectorAll('div')
+const body = document.querySelector('body')
 
-const handleClick = function(e) {
+const boxClick = function(e) {
     this.classList.add('clicked')
 }
 
+const bodyClick = function(e) {
+    if(e.target === e.currentTarget) {
+        boxList.forEach(function(item) {
+            item.classList.remove('clicked')
+        })
+    }
+}
+
 boxList.forEach(function(item) {
-    item.addEventListener('click', handleClick)
+    item.addEventListener('click', boxClick)
 })
+
+body.addEventListener('click', bodyClick)
