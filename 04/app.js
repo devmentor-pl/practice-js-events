6783db8563432cf2721cff1dc54c3fa162ebb36d
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2022-05-02 16:12:46 +0200

    Zadanie nr 4 poprawione

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,27 @@
 const divElements = document.querySelectorAll('div');
-console.log(divElements);
+const bodyElement = document.querySelector('body');
 
-divElements.forEach(div => {
-  div.addEventListener('click', function() {
-    const time = div.dataset.time;
-    setTimeout(() => this.classList.add('clicked'), time)
-  })
+const addClassOnClick = function() {
+
+  console.log(this);
+  const element = this;
+  const timeValue = this.dataset.time;
+
+  function addClass(divEl) {
+    divEl.classList.add('clicked')
+  }
+  setTimeout(addClass, timeValue, element);
+}
+
+
+const removeClassOnClick = function() {
+  divElements.forEach(function(divEl) {
+    divEl.classList.remove('clicked');
+  });
+}
+
+divElements.forEach(function(divEl) {
+  divEl.addEventListener('click', addClassOnClick);
 })
+
+bodyElement.addEventListener('click', removeClassOnClick, true);
