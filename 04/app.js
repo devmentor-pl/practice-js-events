1950ae78067e7535a58b06dc5e568ffd1ae9f278
Dawid Drag <dede7d@wp.pl>
2022-05-03 13:56:38 +0200

    added js code

--- 04/app.js
+++ 04/app.js
@@ -2,15 +2,25 @@ const divList = document.querySelectorAll('div');
 const elementBody = document.querySelector('body')
 
 const addClass = function () {
-    this.classList.add('clicked');
-   
+
+    const self = this;
+
+    setTimeout(function () {
+        self.classList.add('clicked');
+
+    }, self.dataset.time);
+
+
 };
 
 
+
 divList.forEach(function (element) {
+
     element.addEventListener('click', addClass);
 
 });
+
 
 
 const removeClass = function () {
@@ -20,3 +30,5 @@ const removeClass = function () {
 };
 
 elementBody.addEventListener('click', removeClass, true);
+
+
