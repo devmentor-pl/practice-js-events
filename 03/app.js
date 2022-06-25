58d1f27c29b2328dbce431d8089e300034ede9b1
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-06-25 19:58:59 +0200

    task 3 done

---
+++ 03/app.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll("button");
+const clicked = function () {
+	this.innerText = "clicked";
+	console.log("clicked");
+	this.removeEventListener("click", clicked);
+};
+buttons.forEach((button) => {
+	button.addEventListener("click", clicked);
+});
