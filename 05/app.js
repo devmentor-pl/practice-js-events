b0fea2adfa8e1700ec6617b3c87250e12a07e7e1
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-09-06 17:38:19 +0200

    Add countHandler function

--- 05/app.js
+++ 05/app.js
@@ -8,7 +8,36 @@ const stats = {
 };
 
 /* tutaj umieść swój kod */
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
 
+const countHandler = function(e){
+    e.preventDefault();
+
+    if(e.target.className === 'text'){
+        for(const key in stats.paragraphs){
+            if(key === e.target.dataset.id){
+                stats.paragraphs[key]++;
+            }
+            if(!stats.paragraphs[e.target.dataset.id]){
+                stats.paragraphs[e.target.dataset.id] = 1;
+            }
+        }
+    }
+    
+    if(e.target.href){
+        for(const key in stats.links){
+            if(key === e.target.getAttribute('href')){
+                stats.links[key]++
+            }
+            if(!stats.links[e.target.getAttribute('href')]){
+                stats.links[e.target.getAttribute('href')] = 1;
+            }
+        }
+    }
+}
+paragraphs.forEach(function(p){
+    p.addEventListener('click', countHandler)});
 
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
@@ -25,10 +54,13 @@ const fireCustomEvent = function(element, name) {
 
 const renderStats = function(data, element) {
     let html = '';
+    // console.log('data', data);
+    // console.log('element', element);
     for(let elementType in data) {
         html += '<ul>';
-
+        // console.log('data[elementType]', data[elementType]);
         for(let key in data[elementType]) {
+            // console.log('data[elementType][key]', data[elementType][key]);
             
             html += '<li>';
             html += key + ' -> ' + data[elementType][key];
