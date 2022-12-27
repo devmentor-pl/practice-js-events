f8b9379bb81bdfc5b5c9b0570d15ab10642b12e4
Sebastian Kołodziej <sebastiankolodziej87@gmail.com>
2022-12-27 15:44:36 +0100

    finish task 04.02

--- 04/app.js
+++ 04/app.js
@@ -1,12 +1,29 @@
 const divList = document.querySelectorAll('div')
+const body = document.querySelector('body');
+
+function handleClick() {
+    this.classList.add('clicked')
+};
 
 divList.forEach(function(element) {
     element.addEventListener(
         'click',
         handleClick
     )
+    
 });
 
-function handleClick() {
-    this.classList.add('clicked')
+body.addEventListener(
+        'click',
+        subtractionClass
+);
+            
+function subtractionClass(e) {
+    if(e.target === e.currentTarget) {
+        divList.forEach(function(element) {
+            element.classList.remove('clicked')
+        })
+    }
 };
+            
+            
