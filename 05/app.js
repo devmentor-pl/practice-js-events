8ec1a48f5f56c35f4ddf50c107b14fe0aa57900c
Michal Lata <maxpoz3212@gmail.com>
2023-01-01 18:57:50 +0100

    Add task 05 solution

--- 05/app.js
+++ 05/app.js
@@ -8,28 +8,58 @@ const stats = {
 };
 
 /* tutaj umieść swój kod */
+const paragraphList = document.querySelectorAll('p');
 
+paragraphList.forEach(function (paragraph) {
+    paragraph.addEventListener('click', function (e) {
+        const tagName = e.target.tagName;
+
+        if (tagName === 'A') {
+            e.preventDefault();
+            const link = e.target.getAttribute('href');
+            const linkObj = stats.links;
+            let counter = 0;
+
+            if (!isNaN(linkObj[link])) {
+                linkObj[link] = ++linkObj[link];
+            } else {
+                linkObj[link] = ++counter;
+            }
+        } else if (tagName === 'P') {
+            const paragraphID = e.target.dataset.id;
+            const paragraphObj = stats.paragraphs;
+            let counter = 0;
+
+            if (!isNaN(paragraphObj[paragraphID])) {
+                paragraphObj[paragraphID] = ++paragraphObj[paragraphID];
+            } else {
+                paragraphObj[paragraphID] = ++counter;
+            }
+
+        }
+    })
+})
 
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
 const statsElement = document.querySelector('.stats');
-const fireCustomEvent = function(element, name) {
+const fireCustomEvent = function (element, name) {
     console.log(element, '=>', name);
 
     const event = new CustomEvent(name, {
         bubbles: true,
     });
 
-    element.dispatchEvent( event );
+    element.dispatchEvent(event);
 }
 
-const renderStats = function(data, element) {
+const renderStats = function (data, element) {
     let html = '';
-    for(let elementType in data) {
+    for (let elementType in data) {
         html += '<ul>';
 
-        for(let key in data[elementType]) {
-            
+        for (let key in data[elementType]) {
+
             html += '<li>';
             html += key + ' -> ' + data[elementType][key];
             html += '</li>';
@@ -42,9 +72,9 @@ const renderStats = function(data, element) {
 }
 
 
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     const tagName = e.target.tagName;
-    if(tagName.includes('P') || tagName.includes('A')) {
+    if (tagName.includes('P') || tagName.includes('A')) {
         fireCustomEvent(statsElement, 'render');
     }
 });
