714865bc39ba79912912cee11378fb91282414fc
Michal Lata <maxpoz3212@gmail.com>
2023-01-02 12:27:37 +0100

    Add fix

--- 03/app.js
+++ 03/app.js
@@ -5,15 +5,9 @@ const changeText = function () {
     this.innerText = clicked;
     console.log(clicked);
 
-    const clickCheck = this.innerText;
-
-    if (clickCheck === clicked) {
-        this.removeEventListener('click', changeText);
-    }
-
+    this.removeEventListener('click', changeText);
 }
 
 btnList.forEach(function (item) {
     item.addEventListener('click', changeText)
-
 })
