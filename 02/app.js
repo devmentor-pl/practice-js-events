79ae2bd954f021b224aabb6bdaf5ab7c9044b359
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-03-16 11:59:48 +0100

    finish task 2

---
+++ 02/app.js
@@ -0,0 +1,15 @@
+const divDisplay = document.querySelector('div');
+
+if(divDisplay){
+    divDisplay.addEventListener('mouseleave', onLeave);
+    divDisplay.addEventListener('mousemove', onMove);
+}
+
+
+function onLeave() {
+    console.log('mouseleave');
+}
+
+function onMove() {
+    console.log('mousemove');
+}
