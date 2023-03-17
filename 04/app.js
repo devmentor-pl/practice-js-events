4643649f1d068a2a7e25edf82259714e0c5d2ed4
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-03-17 22:57:20 +0100

    finish task 4

---
+++ 04/app.js
@@ -0,0 +1,31 @@
+//4.01
+const divs =  document.querySelectorAll('div');
+const body = document.querySelector('body');
+
+if(divs) {
+    divs.forEach(function(div){
+        div.addEventListener('click', addClass)
+    })    
+}
+
+
+body.addEventListener('click', removeClasses)
+
+
+
+function addClass(e) {
+    const currentElement = e.currentTarget;
+    console.log(currentElement.dataset.time); 
+    setTimeout(function(){
+        console.log(currentElement);
+        currentElement.classList.add('clicked');
+    }, currentElement.dataset.time)    
+}
+
+function removeClasses(e) {
+    if(e.target===e.currentTarget) {
+        divs.forEach(function(div){
+            div.classList.remove('clicked');
+        })
+    }
+}
