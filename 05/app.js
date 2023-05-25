023458e29df435aef3309aec0595141e1a3e555f
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-05-25 16:28:19 +0200

    Task 05 - in progress

--- 05/app.js
+++ 05/app.js
@@ -9,27 +9,57 @@ const stats = {
 
 /* tutaj umieść swój kod */
 
+const links = document.querySelectorAll('.link[href]')
+const paragraphs = document.querySelectorAll('p')
+
+const count = function (key, elements) {
+    const keys = Object.keys(stats[elements])
+
+    if (keys.includes(key)) {
+        stats[elements][key]++
+    } else {
+        stats[elements][key] = 1
+    }
+}
+
+links.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault()
+        const href = link.getAttribute('href')
+
+        count(href, 'links')
+    })
+})
+
+paragraphs.forEach(function (p) {
+    p.addEventListener('click', function (e) {
+        const id = p.dataset.id
+
+        count(id, 'paragraphs')
+    })
+})
+
 
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
 const statsElement = document.querySelector('.stats');
-const fireCustomEvent = function(element, name) {
-    console.log(element, '=>', name);
+const fireCustomEvent = function (element, name) {
+    // console.log(element, '=>', name);
 
     const event = new CustomEvent(name, {
         bubbles: true,
     });
 
-    element.dispatchEvent( event );
+    element.dispatchEvent(event);
 }
 
-const renderStats = function(data, element) {
+const renderStats = function (data, element) {
     let html = '';
-    for(let elementType in data) {
+    for (let elementType in data) {
         html += '<ul>';
 
-        for(let key in data[elementType]) {
-            
+        for (let key in data[elementType]) {
+
             html += '<li>';
             html += key + ' -> ' + data[elementType][key];
             html += '</li>';
@@ -42,9 +72,9 @@ const renderStats = function(data, element) {
 }
 
 
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     const tagName = e.target.tagName;
-    if(tagName.includes('P') || tagName.includes('A')) {
+    if (tagName.includes('P') || tagName.includes('A')) {
         fireCustomEvent(statsElement, 'render');
     }
 });
