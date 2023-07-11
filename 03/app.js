5bc32befcc6319ba7a9e2569ea087fe768d081ca
anetasobanska <sannettka@gmail.com>
2023-07-11 17:45:59 +0200

    finish 03

---
+++ 03/app.js
@@ -0,0 +1,17 @@
+const init = function() {
+    const btns = document.querySelectorAll('button');
+    const onClick = function (e) {
+        const btnClicked = e.target;
+        btnClicked.innerText = 'clicked';
+        console.log(btnClicked, 'clicked');        
+       
+        if(btnClicked.innerText = 'clicked') {
+            btnClicked.removeEventListener('click', onClick);
+        }
+    }
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', onClick);
+    })
+}
+document.addEventListener('DOMContentLoaded', init);
