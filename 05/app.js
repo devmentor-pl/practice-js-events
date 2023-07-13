79ae17bdc56d2ed3ba558d82d27b9fe503448bc7
anetasobanska <sannettka@gmail.com>
2023-07-13 15:36:25 +0200

    finish 05

--- 05/app.js
+++ 05/app.js
@@ -10,11 +10,68 @@ const stats = {
 /* tutaj umieść swój kod */
 
 
+
+const pList = document.querySelectorAll('p');
+const linkList = document.querySelectorAll('a');
+
+pList.forEach(p => {
+    const paragraphValue = p.dataset.id;
+    
+    if (typeof paragraphValue !== 'undefined') {
+        stats.paragraphs[paragraphValue] = 0;
+    }
+    
+})
+
+linkList.forEach(link => {
+    const linkValue = link.getAttribute('href');
+    
+    if (typeof linkValue !== 'undefined') {
+        stats.links[linkValue] = 0;
+    }
+})
+
+const onClick = function (e) {
+    const el = e.target;
+    if (el.hasAttributes('href')) {
+        e.preventDefault();
+    }
+    
+    if(el.dataset.id === 'p1') {
+        ++stats.paragraphs.p1;
+    }
+    if(el.dataset.id === 'p2') {
+        ++stats.paragraphs.p2;
+    }
+    if(el.dataset.id === 'p3') {
+        ++stats.paragraphs.p3;
+    }
+
+    if(el.getAttribute('href') === '/dolor.html') {
+        ++stats.links['/dolor.html'];
+    }
+    if(el.getAttribute('href') === '/adipisicing-elite.html') {
+        ++stats.links['/adipisicing-elite.html'];
+    }
+    if(el.getAttribute('href') === '/consectetur.html') {
+        ++stats.links['/consectetur.html'];
+    }
+
+    console.log(stats);
+}
+
+
+pList.forEach(p => {
+    p.addEventListener('click', onClick);
+});
+
+
+
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
 const statsElement = document.querySelector('.stats');
 const fireCustomEvent = function(element, name) {
-    console.log(element, '=>', name);
+    // console.log(element, '=>', name);
 
     const event = new CustomEvent(name, {
         bubbles: true,
