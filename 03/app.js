b7c1f1bf3f0aaaddc78029bbf43b3584d2e0ab37
Marcin <xcinek1337@wp.pl>
2023-08-03 07:40:38 +0200

    03

--- 03/app.js
+++ 03/app.js
@@ -1,15 +1,11 @@
 const btns = document.querySelectorAll('button');
-let count = 0;
 
 function sayClicked(e) {
-	// console.log(e.target); //obiekt ktory klikam
-
-	if (e.target.tagName === 'BUTTON') {
-		e.target.removeEventListener('click', sayClicked);
-		console.log('clicked');
-	}
+	e.target.removeEventListener('click', sayClicked);
+	console.log('clicked');
 }
-
-btns.forEach(function (btn) {
-	btn.addEventListener('click', sayClicked);
-});
+if (btns) {
+	btns.forEach(function (btn) {
+		btn.addEventListener('click', sayClicked);
+	});
+}
