f8d6909f58398ff3ece15ad8ceb258355f8d7dd1
Agata Witczak <witczakagata123@gmail.com>
2023-09-26 19:12:58 +0200

    04.02 done

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,20 @@
 const divList = document.querySelectorAll('div')
+const body = document.querySelector('body')
 
 const clicked = function (e) {
     e.currentTarget.classList.add('clicked')
 }
 
+const unClicked = function (e) {
+    if (e.target === e.currentTarget) {
+        divList.forEach(function (item) {
+            item.classList.remove('clicked')
+        })
+    }
+}
+
 divList.forEach(function (item) {
     item.addEventListener('click', clicked)
 })
+
+body.addEventListener('click', unClicked)
