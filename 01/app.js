bf5e808d29ad08f99167e489584d5ff12daa4dce
Kaczupinko <kaczupinko@gmail.com>
2023-10-12 16:41:04 +0100

    First task refactor

--- 01/app.js
+++ 01/app.js
@@ -1,10 +1,6 @@
-//szukam elementu button
 const btnE1 = document.querySelector('button');
-//sprawdzam czy znalazlem button
 if(btnE1) {
-    //dodaje nasluchiwanie na klikniecie
     btnE1.addEventListener('click', () => {
-        //wyswietlam w konsoli komunikat
         console.log('clicked');
     })
 }
