456cba778d69ebac3a2b1ca525429bd9f02c6486
Marcin Chechelski <marcin.chechelski@op.pl>
2023-10-30 22:08:17 +0100

    task done

--- 02/app.js
+++ 02/app.js
@@ -1,24 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const div = document.querySelector("div");
-
-//     if (div) {
-//         div.addEventListener("mouseleave", function () {
-//             console.log("opuszczenie elementu przez kursor");
-//         });
-//     }
-// });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const div = document.querySelector("div");
-
-//     if (div) {
-//         div.addEventListener("mouseenter", function () {
-//             for (let i = 0; i < mouseenter; i++) {
-//                 console.log(" poruszanie się kursora nad tym elementem");
-//             }
-//         });
-//     }
-// });
 document.addEventListener("DOMContentLoaded", function () {
     const div = document.querySelector("div");
     div.addEventListener("mousemove", function () {
@@ -29,5 +8,3 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("Kursor opuścił element DIV.");
     });
 });
-
-// Obsługuje zdarzenie "mouseout" (opuszczenie elementu przez kursor)
