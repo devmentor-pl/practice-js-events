6a13f7bc64e62b55aefcc887200fe5197b66f64f
Aleksy Zydlewski <limeakiwano33@gmail.com>
2023-11-20 12:42:19 +0100

    v2 prop

--- 05/app.js
+++ 05/app.js
@@ -10,27 +10,30 @@ const stats = {
 /* tutaj umieść swój kod */
 const linkList = document.querySelectorAll('[href]')
 console.log(linkList);
+linkList.forEach(function (item) {
+    item.addEventListener('click') //tu wiem,ze potrzebuje funkcji, nie jestem tylko pewien czy ona juz nie istnieje w kodzie
+})
 
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
 const statsElement = document.querySelector('.stats');
-const fireCustomEvent = function(element, name) {
+const fireCustomEvent = function (element, name) {
     console.log(element, '=>', name);
 
     const event = new CustomEvent(name, {
         bubbles: true,
     });
 
-    element.dispatchEvent( event );
+    element.dispatchEvent(event);
 }
 
-const renderStats = function(data, element) {
+const renderStats = function (data, element) {
     let html = '';
-    for(let elementType in data) {
+    for (let elementType in data) {
         html += '<ul>';
 
-        for(let key in data[elementType]) {
-            
+        for (let key in data[elementType]) {
+
             html += '<li>';
             html += key + ' -> ' + data[elementType][key];
             html += '</li>';
@@ -43,9 +46,9 @@ const renderStats = function(data, element) {
 }
 
 
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     const tagName = e.target.tagName;
-    if(tagName.includes('P') || tagName.includes('A')) {
+    if (tagName.includes('P') || tagName.includes('A')) {
         fireCustomEvent(statsElement, 'render');
     }
 });
