c02c4d63465f75d31b014d954113f85d764d86d8
Kasia <k.kasia912@gmail.com>
2023-11-28 17:16:07 +0100

    add check statment for btn -task 01

--- 01/app.js
+++ 01/app.js
@@ -3,4 +3,4 @@ const btn = document.querySelector('button');
 function showinfo() {
   console.log('clicked');
 }
-btn.addEventListener('click', showinfo);
+btn && btn.addEventListener('click', showinfo);
