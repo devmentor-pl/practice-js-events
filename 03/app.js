240102c35a12b37f38bb242a731f508057c59cd1
Kamil Lewandowski <kamlew555@gmail.com>
2023-12-28 11:03:22 +0100

    delete if statement and add removeEventListener method

--- 03/app.js
+++ 03/app.js
@@ -2,13 +2,10 @@ const buttonList = document.querySelectorAll("button");
 console.log(buttonList);
 
 const handleClick = (e) => {
-	if (!e.target.clicked) {
-		e.currentTarget.textContent = "clicked";
-        console.log("clicked");
-        console.log( e.currentTarget.clicked = true);
-	}else{
-        console.log((e.currentTarget.clicked = false));
-    }
+	e.currentTarget.textContent = "clicked"
+    console.log('clicked')
+
+    e.currentTarget.removeEventListener('click', handleClick)
 };
 
 buttonList.forEach((btn) => {
