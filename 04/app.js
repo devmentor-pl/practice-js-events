0fbafe6ab606f6946bfcba2ab4d4529a1c5968ce
Dawidtk <dawidtk@gmail.com>
2023-12-29 11:36:04 +0100

    class clicked

--- 04/app.js
+++ 04/app.js
@@ -1,3 +1,13 @@
 const divEl = document.querySelectorAll("div");
 const bodyEl = document.querySelector("body");
 
+const addCliClass = function () {
+    const el = this;
+    const time = el.dataset.time;
+
+    setTimeout(function () {
+        el.classList.add("clicked");
+    
+    }, time);
+};
+
