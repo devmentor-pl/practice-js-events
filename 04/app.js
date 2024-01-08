c9d56610014ad2d69a64cac13937672abeda70c9
Robert Staniszewski <robstani.tech@gmail.com>
2024-01-08 02:13:25 +0100

    Listen for click on 'body' to remove 'clicked' class from 'divs'

--- 04/app.js
+++ 04/app.js
@@ -1,3 +1,5 @@
+// #4.01
+
 const divEls = document.querySelectorAll('div');
 
 divEls.forEach(function(element) {
@@ -7,6 +9,22 @@ divEls.forEach(function(element) {
         if (!this.classList.contains('clicked')) {
             // Dodaj klase 'clicked' na aktualnie kliknietym elemencie.
             this.classList.add('clicked');
+            console.log("Dodano klase clicked");
         }
     });
+});
+
+// #4.02
+
+const bodyEl = document.querySelector('body');
+
+bodyEl.addEventListener('click', function(event) {
+    // Sprawdź, czy kliknięcie nastąpiło bezpośrednio na body, a nie na potomku
+    if (event.target === bodyEl) {
+        // Usuń klasę 'clicked' ze wszystkich elementów div
+        divEls.forEach(function(element) {
+            element.classList.remove('clicked');
+            console.log("Usunięto klase clicked");
+        });
+    }
 });
