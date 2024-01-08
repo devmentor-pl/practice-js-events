3f3583a3be468734a9c1a95558626fee64bfb752
Robert Staniszewski <robstani.tech@gmail.com>
2024-01-08 03:02:32 +0100

    Add a time delay effect between the click event and color change.

--- 04/app.js
+++ 04/app.js
@@ -1,30 +1,47 @@
-// #4.01
-
 const divEls = document.querySelectorAll('div');
 
+// Iteruję przez wszystkie elementy 'div'.
 divEls.forEach(function(element) {
+    
     // Dodaj nasłuchiwanie zdarzenia 'click' na elementach 'div'.
     element.addEventListener('click', function() {
-        // Jeśli element nie posiada klasy 'clicked'
+        
+        // Jeśli element nie posiada klasy 'clicked', to wykonaj następujące:
         if (!this.classList.contains('clicked')) {
+            
+            // Pobierz opóźnienie zapisane w atrybucie 'data-time'.
+            const delay = parseInt(this.dataset.time);
+            
+            // Zachowaj kontekst this.
+            const self = this; 
+            
+            // Ustaw opóźnioną funkcję.
+            setTimeout(function() { 
+            
             // Dodaj klase 'clicked' na aktualnie kliknietym elemencie.
-            this.classList.add('clicked');
-            console.log("Dodano klase clicked");
+            self.classList.add('clicked');
+            }, delay);
         }
     });
 });
 
-// #4.02
-
 const bodyEl = document.querySelector('body');
+// Dodaj nasłuchiwanie zdarzenia 'click' na elemencie body.
 
 bodyEl.addEventListener('click', function(event) {
-    // Sprawdź, czy kliknięcie nastąpiło bezpośrednio na body, a nie na potomku
+    
+    // Sprawdź, czy kliknięcie nastąpiło bezpośrednio na body, a nie na potomku.
     if (event.target === bodyEl) {
-        // Usuń klasę 'clicked' ze wszystkich elementów div
+        
+        // Usuń klasę 'clicked' ze wszystkich elementów div.
         divEls.forEach(function(element) {
             element.classList.remove('clicked');
-            console.log("Usunięto klase clicked");
         });
     }
 });
+
+
+
+
+
+
