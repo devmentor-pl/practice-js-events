95ab4598826b9f2b7550dce5a46bb9cff488f582
Typee8 <wojciechjasiak1324@gmail.com>
2024-02-01 10:29:57 +0100

    Finish 05

--- 05/app.js
+++ 05/app.js
@@ -1,53 +1,67 @@
 const stats = {
-    paragraphs: {
-        'p1': 0,
-    },
-    links: {
-        '/dolor.html': 0,
-    }
+  paragraphs: {
+    p1: 0,
+  },
+  links: {
+    "/dolor.html": 0,
+  },
 };
 
 /* tutaj umieść swój kod */
+const everyP = Array.from(document.querySelectorAll("p"));
 
+everyP.forEach(function (element) {
+  element.addEventListener("click", function (e) {
+    if (e.target.getAttribute("data-id") === "p1") {
+      stats.paragraphs["p1"]++;
+    } else if (e.target.getAttribute("href") === "/dolor.html") {
+      e.preventDefault();
+      stats.links["/dolor.html"]++;
+    }
+  });
+});
 
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
-const statsElement = document.querySelector('.stats');
-const fireCustomEvent = function(element, name) {
-    console.log(element, '=>', name);
+const statsElement = document.querySelector(".stats");
+const fireCustomEvent = function (element, name) {
+  console.log(element, "=>", name);
 
-    const event = new CustomEvent(name, {
-        bubbles: true,
-    });
+  const event = new CustomEvent(name, {
+    bubbles: true,
+  });
 
-    element.dispatchEvent( event );
-}
+  element.dispatchEvent(event);
+};
 
-const renderStats = function(data, element) {
-    let html = '';
-    for(let elementType in data) {
-        html += '<ul>';
+const renderStats = function (data, element) {
+  let html = "";
+  for (let elementType in data) {
+    html += "<ul>";
 
-        for(let key in data[elementType]) {
-            
-            html += '<li>';
-            html += key + ' -> ' + data[elementType][key];
-            html += '</li>';
-        }
-
-        html += '</ul>'
+    for (let key in data[elementType]) {
+      html += "<li>";
+      html += key + " -> " + data[elementType][key];
+      html += "</li>";
     }
 
-    element.innerHTML = html;
-}
+    html += "</ul>";
+  }
 
+  element.innerHTML = html;
+};
 
-document.addEventListener('click', function(e) {
-    const tagName = e.target.tagName;
-    if(tagName.includes('P') || tagName.includes('A')) {
-        fireCustomEvent(statsElement, 'render');
-    }
+document.addEventListener("click", function (e) {
+  const tagName = e.target.tagName;
+  if (tagName.includes("P") || tagName.includes("A")) {
+    fireCustomEvent(statsElement, "render");
+  }
 });
-statsElement.addEventListener('render', renderStats.bind(this, stats, statsElement));
-document.addEventListener('DOMContentLoaded', fireCustomEvent.bind(null, statsElement, 'render'));
-
+statsElement.addEventListener(
+  "render",
+  renderStats.bind(this, stats, statsElement)
+);
+document.addEventListener(
+  "DOMContentLoaded",
+  fireCustomEvent.bind(null, statsElement, "render")
+);
