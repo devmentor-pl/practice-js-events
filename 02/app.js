ee27d34703119a13a2a743a60c1d1a0a210af78e
Typee8 <wojciechjasiak1324@gmail.com>
2024-02-08 13:22:41 +0100

    Add if (div)

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,9 @@
-const div = document.querySelector('div');
-
-div.addEventListener('mousemove', function () {
-  console.log('mouse moving on the square');
-})
-div.addEventListener('mouseleave', function () {
-  console.log('mouse leaves the square');
-})
+const div = document.querySelector("div");
+if (div) {
+  div.addEventListener("mousemove", function () {
+    console.log("mouse moving on the square");
+  });
+  div.addEventListener("mouseleave", function () {
+    console.log("mouse leaves the square");
+  });
+}
