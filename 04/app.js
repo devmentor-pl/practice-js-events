18c330cd62eda154bdacb1b383323064fb11b04a
Typee8 <wojciechjasiak1324@gmail.com>
2024-02-08 13:46:39 +0100

    Simplifie code, Use e.stopPropagation() in colorSquare to prevent clicking on blue change another element to red

--- 04/app.js
+++ 04/app.js
@@ -1,7 +1,9 @@
 const divList = document.querySelectorAll("div");
 const body = document.querySelector("body");
 
-divList.forEach(colorSquares);
+divList.forEach(element => {
+  element.addEventListener("click", colorSquare);
+});
 
 body.addEventListener("click", function (e) {
   if (e.target !== this) {
@@ -12,26 +14,12 @@ body.addEventListener("click", function (e) {
   });
 });
 
-function colorSquares(element) {
-  if (element.getAttribute("class") === "child") {
-    element.addEventListener("click", ColorAllSquares);
-  } else {
-    element.addEventListener("click", ColorTheSquare);
+function colorSquare(e) {
+
+  if(e.target.getAttribute('class') === 'parent') {
+    e.stopPropagation();
   }
-}
 
-function ColorAllSquares(e) {
-  e.stopPropagation();
-  divList.forEach(function (ele) {
-    const timer = ele.dataset.time;
-    setTimeout(() => {
-      ele.classList.add("clicked");
-    }, timer);
-  });
-}
-
-function ColorTheSquare(e) {
-  e.stopPropagation();
   const timer = this.dataset.time;
   setTimeout(() => {
     this.classList.add("clicked");
