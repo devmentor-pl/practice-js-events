4f1c8d850d094afffd1981d96d2a1ee9a4a2f5ce
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-03-03 22:16:08 +0100

    03 all done

--- 03/app.js
+++ 03/app.js
@@ -1,15 +1,12 @@
-//nie działa zatrzymanie EventList..
+//DZIAŁA
 const button = document.querySelectorAll('button');
 
 button.forEach(function(el) {
     el.addEventListener('click', transformButton); 
-    // zatrzymanie po kliknięciu w przycisk
-    if(el.innerText === 'clicked') {
-        el.removeEventListener('click', transformButton);
-    }
 })
 
 function transformButton() {
     this.innerText = 'clicked'; 
     console.log('clicked');
+    this.removeEventListener('click', transformButton);
 }
