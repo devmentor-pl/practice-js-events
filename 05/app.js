f55ff63ea391101920ad30a58ae672123e8393a8
MarcinMorgan <morgangzit@gmail.com>
2024-04-20 16:27:35 +0200

    feat: t5 WIP count clicks on paragraph and link without reloading the page

--- 05/app.js
+++ 05/app.js
@@ -9,6 +9,36 @@ const stats = {
 
 /* tutaj umieść swój kod */
 
+const linkList = document.querySelectorAll('.link');
+const textList = document.querySelectorAll('.text');
+const testText = 'links';
+
+const getHref = function(e) {
+    e.preventDefault();
+    if(typeof(stats.links[this.getAttribute('href')]) === 'number'){
+        stats.links[this.getAttribute('href')] += 1;
+    }
+    else {
+        stats.links[this.getAttribute('href')] = 0;
+        stats.links[this.getAttribute('href')] += 1;
+    }
+}
+
+linkList.forEach(function(element){
+    element.addEventListener('click', getHref);
+})
+
+textList.forEach(function(element) {
+    element.addEventListener('click', function() {
+        if(typeof(stats.paragraphs[this.dataset.id]) === 'number'){
+            stats.paragraphs[this.dataset.id] += 1;
+        }
+        else {
+            stats.paragraphs[this.dataset.id] = 0;
+            stats.paragraphs[this.dataset.id] += 1;
+        }
+    })
+})
 
 /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
