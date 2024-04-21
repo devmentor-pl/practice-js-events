d445cec6018178602a584ee88365c94a4f26bb01
Jarek Stelmach <j.stelmach@onet.eu>
2024-04-21 22:48:36 +0200

    Finished 2 part in ex4 in JS Events

--- 04/app.js
+++ 04/app.js
@@ -3,21 +3,20 @@ const divElements = document.querySelectorAll('div')
 
 // #04.01
 divElements.forEach(function(el){
-    el.addEventListener('click', function() {
-        this.className = 'clicked'
+    el.addEventListener('click', function(event) {
+        event.stopPropagation()
+        this.classList.add('clicked')
     })
 })
 
 // #04.02
 
 document.body.addEventListener('click', function(){
-    if (divElements.forEach(function(el){
-        el.classList.contains('clicked')
-    })) {
-        el.classList.add =
-    }
-
-
+    divElements.forEach(function(el){
+        if(el.classList.contains('clicked')) {
+            el.classList.remove('clicked')
+        }
+    })
 })
 
 
