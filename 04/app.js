5004ea150476efa5f043e94c128ac71f22e438c4
Jaro <jaroswidnik@interia.pl>
2024-04-22 09:46:51 +0200

    Fisnished some part of ex4 in JS Events about setTimeout

--- 04/app.js
+++ 04/app.js
@@ -3,10 +3,23 @@ const divElements = document.querySelectorAll('div')
 
 // #04.01
 divElements.forEach(function(el){
-    el.addEventListener('click', function(event) {
+
+    const adClassFunction = function(event) {
         this.classList.add('clicked')
         event.stopPropagation()
-    })
+    }
+
+    const timeOutfunction = function(e) {
+        const properThis = this;
+
+        const addClassCallFunction = function() {
+            adClassFunction.call(properThis, e)
+        }
+
+       setTimeout(addClassCallFunction, 2000);
+    }
+
+    el.addEventListener('click', timeOutfunction)
 })
 
 // #04.02
@@ -19,7 +32,7 @@ document.body.addEventListener('click', function(){
     })
 })
 
-// #04.04
+
 
 
 
