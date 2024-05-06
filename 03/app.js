6cd2d36030758b74eff3adbba9317d996386e390
Rafal <rafk528@gmail.com>
2024-05-06 21:18:28 +0000

    once: true

--- 03/app.js
+++ 03/app.js
@@ -1,12 +1,26 @@
 const buttons = document.querySelectorAll("button")
 
 buttons.forEach((button) => {
-  button.addEventListener(
-    "click",
-    function () {
-      console.log("clicked")
-      button.textContent = "clicked"
-    },
-    { once: true }
-  )
+  const handleClick = function () {
+    console.log("clicked")
+    button.textContent = "clicked"
+    button.removeEventListener("click", handleClick)
+  }
+
+  button.addEventListener("click", handleClick)
 })
+
+/* Old code */
+// const buttons = document.querySelectorAll("button")
+
+// buttons.forEach((button) => {
+//   button.addEventListener(
+//     "click",
+//     function () {
+//       console.log("clicked")
+//       button.textContent = "clicked"
+//     },
+      //funkcja nieobsługiwana "once" event listener option
+//     { once: true }
+//   )
+// })
