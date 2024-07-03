4bfede8837b9229f923b0b5dc00c93a9107de9da
Sebastian Pietrzykowski <sebpie84@gmail.com>
2024-07-03 16:17:55 +0200

    exercises done

--- 05/app.js
+++ 05/app.js
@@ -1,53 +1,89 @@
-const stats = {
-    paragraphs: {
-        'p1': 0,
-    },
-    links: {
-        '/dolor.html': 0,
-    }
-};
+// const stats = {
+//     paragraphs: {
+//         'p1': 0,
+//     },
+//     links: {
+//         '/dolor.html': 0,
+//     }
+// };
 
-/* tutaj umieść swój kod */
+// /* tutaj umieść swój kod */
+// const paragraph = document.querySelectorAll('p')
 
+// paragraph.forEach(function (item) {
+//     item.addEventListener('click', function (e) {
+//         console.log('ok');
 
-/* nie modyfikuj kodu poniżej, ale przeanalizuj go */
+//         e.preventDefault()
+//         // console.log(e.target, e.currentTarget);
 
-const statsElement = document.querySelector('.stats');
-const fireCustomEvent = function(element, name) {
-    console.log(element, '=>', name);
+//         const tag = e.target.tagName
+//         if (tag === 'P') {
+//             const id = e.target.dataset.id
+//             if (!stats.paragraphs[id]) {
+//                 stats.paragraphs[id] = 0
+//             } 
+//             stats.paragraphs[id] = stats.paragraphs[id] +1
+//         } else {
 
-    const event = new CustomEvent(name, {
-        bubbles: true,
-    });
+//             const href = e.target.getAttribute('href')
+//             stats.links[href] = stats.links[href] + 1
+//             if (!stats.links[href]) {
+//                 stats.links[href] = 0
+//             }
+//             console.log(href);
+//         }
+//         console.log(tag);
+//         // stats.paragraphs.p1 = stats.paragraphs.p1 +1
+//     })
+// })
 
-    element.dispatchEvent( event );
-}
+// /* nie modyfikuj kodu poniżej, ale przeanalizuj go */
 
-const renderStats = function(data, element) {
-    let html = '';
-    for(let elementType in data) {
-        html += '<ul>';
+// const statsElement = document.querySelector('.stats');
+// const fireCustomEvent = function(element, name) {
+//     console.log(element, '=>', name);
 
-        for(let key in data[elementType]) {
+//     const event = new CustomEvent(name, {
+//         bubbles: true,
+//     });
+
+//     element.dispatchEvent( event );
+// }
+
+// const renderStats = function(data, element) {
+//     let html = '';
+//     for(let elementType in data) {
+//         html += '<ul>';
+
+//         for(let key in data[elementType]) {
             
-            html += '<li>';
-            html += key + ' -> ' + data[elementType][key];
-            html += '</li>';
-        }
+//             html += '<li>';
+//             html += key + ' -> ' + data[elementType][key];
+//             html += '</li>';
+//         }
 
-        html += '</ul>'
-    }
+//         html += '</ul>'
+//     }
 
-    element.innerHTML = html;
+//     element.innerHTML = html;
+// }
+
+
+// document.addEventListener('click', function(e) {
+//     const tagName = e.target.tagName;
+//     if(tagName.includes('P') || tagName.includes('A')) {
+//         fireCustomEvent(statsElement, 'render');
+//     }
+// });
+// statsElement.addEventListener('render', renderStats.bind(this, stats, statsElement));
+// document.addEventListener('DOMContentLoaded', fireCustomEvent.bind(null, statsElement, 'render'));
+
+function test(num) {
+    console.log(this);
+    console.log(num);
 }
 
+const copy = test.bind('cos tam', 24)
 
-document.addEventListener('click', function(e) {
-    const tagName = e.target.tagName;
-    if(tagName.includes('P') || tagName.includes('A')) {
-        fireCustomEvent(statsElement, 'render');
-    }
-});
-statsElement.addEventListener('render', renderStats.bind(this, stats, statsElement));
-document.addEventListener('DOMContentLoaded', fireCustomEvent.bind(null, statsElement, 'render'));
-
+copy()
