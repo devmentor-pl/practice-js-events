656dab9b36bea7a98394dc5440b5731aef8e3ee3
Magdalena Grzesik <magdalena.grzersikk@gmail.com>
2024-08-06 17:53:43 +0200

    Add task 01

---
+++ 01/app.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button')
+
+if (button){
+    button.addEventListener('click', clicked)
+}
+
+function clicked() {
+    console.log('button was clicked');    
+}
