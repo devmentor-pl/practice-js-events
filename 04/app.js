b689fdff648715fc1d7f8a99b5849a09154427f0
marrcelp <marcel.piaszczyk@gmail.com>
2025-01-06 20:45:41 +0100

    add timeout

--- 04/app.js
+++ 04/app.js
@@ -1,26 +1,37 @@
 //04.01
 
 const divList = document.querySelectorAll('div');
+const delay = 2000;
 
 const handleClick = function(e){
-    e.stopPropagation();
-    this.classList.add('clicked');
+    // e.stopPropagation();
+    setTimeout(() => {
+        this.classList.add('clicked');
+    }, delay);
+    
 }
 
 divList.forEach((div) => {
+    div.dataset.delay = 2000;
     div.addEventListener('click', handleClick);
     
 })
+
+
 
 //04.02
 
 const body = document.querySelector('body');
 
-const removeClass = function(){
-    const clickedElements = document.querySelectorAll('.clicked');
-    clickedElements.forEach((div) => {
-        div.classList.remove('clicked');
-    })
+const removeClass = function(e){
+    if (e.target === this){
+        const clickedElements = document.querySelectorAll('.clicked');
+        clickedElements.forEach((div) => {
+            div.classList.remove('clicked');
+        })
+    }
 }
 
 body.addEventListener('click', removeClass);
+
+
