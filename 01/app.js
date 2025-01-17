1acdf387328c5499b1d40b6501e91003076af2c1
marrcelp <marcel.piaszczyk@gmail.com>
2025-01-17 21:33:36 +0100

    add condition - if btn

--- 01/app.js
+++ 01/app.js
@@ -1,7 +1,11 @@
 const btn = document.querySelector('button');
 
+if (btn){
+    
 const handleClick = function(){
     console.log('clicked');
 }
 
 btn.addEventListener('click', handleClick);
+
+}
