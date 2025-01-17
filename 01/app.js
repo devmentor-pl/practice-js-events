cd4a2df8ff526d66a5a62f59e6e9bfffe8b0c59c
marrcelp <marcel.piaszczyk@gmail.com>
2025-01-17 21:35:42 +0100

    fix spacing

--- 01/app.js
+++ 01/app.js
@@ -1,11 +1,11 @@
 const btn = document.querySelector('button');
 
 if (btn){
-    
-const handleClick = function(){
-    console.log('clicked');
-}
 
-btn.addEventListener('click', handleClick);
+    const handleClick = function(){
+        console.log('clicked');
+    }
+
+    btn.addEventListener('click', handleClick);
 
 }
