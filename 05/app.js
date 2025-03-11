4c1b0338e7b2883924b6ead3362d7fa0abf73c67
Michał <m.majewski.dm@gmail.com>
2025-03-11 23:36:21 +0100

    iterating all paragraphs, added click event

--- 05/app.js
+++ 05/app.js
@@ -11,6 +11,36 @@ const stats = {
 
 const paragraphs = document.querySelectorAll('p')
 
+paragraphs.forEach(function(paragraph) {
+    paragraph.addEventListener('click', function(event) {
+        if(event.target.classList.contains('link')) {
+            event.preventDefault()
+            const linkHref  = event.target.getAttribute('href')
+
+
+            if(!stats.links.hasOwnProperty(linkHref)) {
+                stats.links[linkHref] = 0
+            }
+
+            stats.links[linkHref]++
+
+        } else if (event.target.classList.contains('text')) {
+            const paragraphId = event.target.dataset.id 
+            
+            if(!stats.paragraphs.hasOwnProperty(paragraphId)) {
+                stats.paragraphs[paragraphId] = 0;
+            }
+
+            stats.paragraphs[paragraphId]++ 
+        } else {
+            if(event.target.parentElement.classList.contains('text')) {
+                const paragraphId = event.target.parentElement.dataset.id 
+                
+                stats.paragraphs[paragraphId]++}
+        }
+
+    })
+})
 
 
 
