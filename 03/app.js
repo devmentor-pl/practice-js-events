1cf5702f9cd624fe2232271b5bede65e05bdb74e
Zaneta Sochon <twj email>
2025-05-27 15:57:04 +0200

    fix(3): Remove useless condition

--- 03/app.js
+++ 03/app.js
@@ -1,11 +1,9 @@
 const btnList = document.querySelectorAll('button');
 
 const handleClick = function (e) {
-  if (e.target === e.currentTarget) {
     e.target.textContent = 'clicked';
     console.log('clicked');
     e.target.removeEventListener('click', handleClick);
-  }
 };
 
 const btn = btnList.forEach((el) => {
