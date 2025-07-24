29b974a94c2b9d830701bb40ced8edffae22cf91
Michal Wasiak <michal.wasiak@hotmail.com>
2025-07-24 20:51:50 +1000

    Task 01 done

---
+++ 01/app.js
@@ -0,0 +1,7 @@
+const btnEl = document.querySelector('button');
+// console.log(btnEl);
+if (btnEl) {
+    btnEl.addEventListener('click', function () {
+        console.log('button was clicked!');
+    })
+}
