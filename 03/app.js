06e91df42c29224f2fb1dfcb67949f218f4084fa
Michal Wasiak <michal.wasiak@hotmail.com>
2025-08-04 15:15:15 +1000

    Task 03 - if statement removed

--- 03/app.js
+++ 03/app.js
@@ -3,9 +3,9 @@ const btnsList = document.querySelectorAll('button');
 const changeText = function (e) {
     this.innerText = 'clicked';
     console.log(this.innerText);
-    if (this.innerText === 'clicked') {
+   
         this.removeEventListener('click', changeText, true);
-    }
+    
 }
 
 
